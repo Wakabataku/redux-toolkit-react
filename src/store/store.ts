55a3758dc4a0237e9023895152b3bb6fd87914e9
Wakabataku <wakaba_taku666@yahoo.co.jp>
2022-04-29 07:33:01 +0900

    add zennApiSelector with reference to the official document

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,11 +1,13 @@
-import { configureStore } from "@reduxjs/toolkit"
+import { configureStore, createSelector } from "@reduxjs/toolkit"
 import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux"
 
 import counterReducer from "./counterSlice"
+import zennApiReducer from "./zennApiSlice"
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
+    zennApi: zennApiReducer,
   },
 })
 
@@ -14,3 +16,8 @@ export type AppDispatch = typeof store.dispatch
 
 export const useAppDispatch = () => useDispatch<AppDispatch>()
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector
+
+const zennApiSelector = (state: RootState) => state.zennApi
+export const zennTrendSelector = createSelector(zennApiSelector, (zenn) => {
+  return zenn.trends
+})
