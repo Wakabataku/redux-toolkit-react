a03e621741555876f4da35a7c169f4882f7d7406
Wakabataku <wakaba_taku666@yahoo.co.jp>
2022-06-20 21:30:11 +0900

    modify index.ts to persist redux

--- src/index.tsx
+++ src/index.tsx
@@ -3,15 +3,21 @@ import ReactDOM from "react-dom/client"
 import "./index.css"
 import reportWebVitals from "./reportWebVitals"
 import { Provider } from "react-redux"
+import { PersistGate } from "redux-persist/integration/react"
+import { persistStore } from "redux-persist"
 
 import { store } from "./store/store"
 import AsyncApp from "./AsyncApp"
+
+const persistor = persistStore(store)
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement)
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <AsyncApp />
+      <PersistGate loading={null} persistor={persistor}>
+        <AsyncApp />
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 )
