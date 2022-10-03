665f92d6014516e566c223c489a94779523962a2
Wakabataku <wakaba_taku666@yahoo.co.jp>
2022-10-03 22:23:07 +0900

    feat: AsyncAppの途中までコメント追加

--- src/AsyncApp.tsx
+++ src/AsyncApp.tsx
@@ -1,29 +1,64 @@
+// .tsxでは，Reactを必ず読み込む
 import React from "react"
+// storeで作成したuseAppSelectorとuseAppDIspatchをimport
+// ここら辺は，redux selectorとかdispatchでググると出てくる
 import { AppDispatch, useAppSelector } from "./store/store"
 import { useDispatch } from "react-redux"
+// reduxでは，actionをdispatchする
+// dispatchすることで，store内で変数を使えるようになる
+// index.tsxでくくったProvider内で使えるともいう
+// AAsyncAppはそこに含まれているので，useAppSelectorでstoreにアクセスできる
 import { addition, subtraction } from "./store/counterSlice"
 // このBoxは気にしなくていい
 import { Box } from "@mui/material"
 
+// 非同期のaction
+// reduxでは，非同期で変数をセットするためのactionをdispatchできる
+// 何度も書くけど，actionをdispatchすることで，store内で変数を使える
+// a=1でも，新たにa=2をactionとして，dispatchすることで，
+// 次回からは，selectorを使って，a=2を取得できる
 import { twoAsyncCount } from "./store/asyncCounterSlice"
+// resux-persistの話．優先度低い
 import { persistor } from "./store/store"
+// reduxでは，配列もdispatchできる
+// 配列のdispatch方法は工夫が必要なので，arrayCoutnerのcreateSlice内，arrayAdditionを参照
 import { arrayAddition } from "./store/arrayCounter"
 
+// typescriptでは，返り値に型を指定できる
+// 1とか2はnumber型，あいうえお，abcはstring型とtypescript上で定められている．変数のとなりにコロン(:)があれば，型を意味している
+// 今回のAsyncAppはReact.FC型．Reactをつくっている人が新たに型を宣言して，作ってくれたんだね
+// また，jsvascriptやtypescriptでは，アロー関数という書き方をすることが多い．ぶっちゃけ好み
+//function minus(a, b) {return a-b}は，const minus = (a, b) => {return a-b}と同じ意味．習うより慣れろ感すごい
 const AsyncApp: React.FC = () => {
+  // これがselector
+  // state.counter.countOneという変数にアクセスしている
+  // 別にAsyncAppは何も引数とっていないのに，グローバル変数みたいに使えるなんて，便利
+  // グローバル変数と違って，selectorを使わなければ，読み込まれないから軽い
   const count = useAppSelector((state) => state.counter.countOne)
   const twoCount = useAppSelector((state) => state.twoCounter)
   const arrayCount = useAppSelector((state) => state.arrayCounter.arrayCount)
+  // dispatchするための関数
   const dispatch: AppDispatch = useDispatch()
 
+  // redux-persistの話
+  // 無視
   const onClickPurge = () => {
     console.log("localdataをpurge")
     persistor.purge()
   }
 
   return (
+    // これらの<></>はhtmltの書き方．html, cssを知っていると楽
     <div className="App">
       <Box sx={{ m: 2 }}>
         <h1>Count: {count}</h1>
+        {/* ボタンコンポーネント onClickでタッチしたりクリックしたときの挙動を定義している
+          () => 何とか~    となっているのは，クリックされたら引数をとらない関数で，「何とか」という処理をしてねという意味
+          今回は，dispatchしてねということ
+          何を？addition(~)を中身は1です
+          つまり，additionという動作をしてね．そのときに使う変数は1だよということ
+          今回のadditionはプラスなので+1となる．counterSlicew参照
+        */}
         <button onClick={() => dispatch(addition(1))}>Up</button>
         <button onClick={() => dispatch(subtraction(1))}>Down</button>
         <button onClick={() => onClickPurge()}>Purge</button>
@@ -31,6 +66,7 @@ const AsyncApp: React.FC = () => {
       <Box sx={{ m: 3 }}>
         <h1>Async Count: {twoCount.count}</h1>
         <h1>Async Message: {twoCount.message}</h1>
+        {/* とりあえずここまで，後は随時追加していく */}
         <button
           onClick={() =>
             dispatch(
--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,44 @@
+// Reactをimportしないと.tsxは動かない
 import React from "react"
+// こいつは俺も知らん
 import ReactDOM from "react-dom/client"
+// スタイルファイル
 import "./index.css"
+// 知らん
 import reportWebVitals from "./reportWebVitals"
+// reduxという機能を使うためのもの
+// Providerでくくると，その中でreduxという機能が使えるようになる
 import { Provider } from "react-redux"
+// redux-persistを使うためのもの
 import { PersistGate } from "redux-persist/integration/react"
 
+// reduxで使うstoreをstore/store.tsで作成しているため，それをimpoetしている
 import { store, persistor } from "./store/store"
+// Reactコンポーネント，コンポーネントというのは，Webページの一部（検索フォームとかボタンとか）のこと．
+// おれがつくった
 import AsyncApp from "./AsyncApp"
 
 // index, AsyncApp.tsx, storeフォルダ内が理解できたら完璧
 // 最初は，何がなんだかわからないから，なんでも質問して
 // 基礎ではあるけど，初心者がやるようなチュートリアルではないから
+// ///////////////////////////////////////////////////////////
+// ///////////////////////////////////////////////////////////
+// vscodeのターミナルbashでもなんでもいいが，npm run startでlocalhost:300が立ち上がり動作が見れる
+// ///////////////////////////////////////////////////////////
+// //////////////////////////////////////////////////////////
+
+// 知らん
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement)
 root.render(
+  // Reactのstrictモードを使うということ
+  // こいつでくくるとstrictモードを使うということ，基本的にこれだから，それ以外のモードは知らない
   <React.StrictMode>
+    {/* このProviderでくくるとそのなかで，reduxのstoreを使えるようになる
+      storeというのは，変数の集まりみたいなもので，function(a, b){}のようにいちいち引数を受け取る方式で関数を書かなくて良くなる
+      例えば，function(){ a = useAppSelector(state => state.a)}のようにuseSelectorを使うことで変数を取得できる
+    */}
     <Provider store={store}>
+      {/* redux-persistを使うためのもの．こいつの理解優先度はかなり低い．無視． */}
       <PersistGate loading={null} persistor={persistor}>
         <AsyncApp />
       </PersistGate>
