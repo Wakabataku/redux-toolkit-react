ae421d63a4deba9032e98679b0d0856630060113
Wakabataku <wakaba_taku666@yahoo.co.jp>
2022-10-03 21:54:43 +0900

    コメント追加

--- src/index.tsx
+++ src/index.tsx
@@ -8,6 +8,9 @@ import { PersistGate } from "redux-persist/integration/react"
 import { store, persistor } from "./store/store"
 import AsyncApp from "./AsyncApp"
 
+// index, AsyncApp.tsx, storeフォルダ内が理解できたら完璧
+// 最初は，何がなんだかわからないから，なんでも質問して
+// 基礎ではあるけど，初心者がやるようなチュートリアルではないから
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement)
 root.render(
   <React.StrictMode>
